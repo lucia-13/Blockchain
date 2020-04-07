ec7f142ae911fe9046221c39e09ef64c89f35e9b
lucia-13 <lucyjump@hotmail.com>
2020-04-07 01:26:37 -0500

    extended testing + read me

--- blockchain.py
+++ blockchain.py
@@ -9,37 +9,42 @@ class Blockchain:
         self.chain = []
         self.unconfirmed_transactions = []
         self.genesis_block()
-
+#conmstructor of a defualt genesis block, that pints to a 0 previous_hash
     def genesis_block(self):
         transactions = []
         genesis_block = Block(transactions, "0")
         genesis_block.generate_hash()
         self.chain.append(genesis_block)
 
+#fucntion to add block to the tail fo the blockchain, implements generate_hash fucntion for generating hash for the new block,
+    # uses proof of work to check for validity
     def add_block(self, transactions):
-        previous_hash = (self.chain[len(self.chain) - 1]).hash
-        new_block = Block(transactions, previous_hash)
-        new_block.generate_hash()
-        # proof = proof_of_work(block)
+        previous_block_hash = self.chain[len(self.chain) - 1].hash
+        new_block = Block(transactions, previous_block_hash)
+        proof = self.proof_of_work(new_block)
         self.chain.append(new_block)
-
+        return proof, new_block
+#print whole blockchain using block print_contents() fucntion
     def print_blocks(self):
         for i in range(len(self.chain)):
             current_block = self.chain[i]
             print("Block {} {}".format(i, current_block))
             current_block.print_contents()
-
+#check if blockchin is valid, by comparing cached hash of current block with updated hash, then comapring
+    # previous block's updated hash with the current's block previous hash
     def validate_chain(self):
         for i in range(1, len(self.chain)):
             current = self.chain[i]
             previous = self.chain[i - 1]
-            if (current.hash != current.generate_hash()):
+            if current.hash != current.generate_hash():
                 print("Current hash does not equal generated hash")
                 return False
-            if (current.previous_hash != previous.generate_hash()):
+            if current.previous_hash != previous.generate_hash():
                 print("Previous block's hash got changed")
                 return False
         return True
+#function implementating of work, iterates toll the nonce(0 till succesfull)
+    # used for the hash generation creates a hash with 2(difficulty variable default) leading 0s
 
     def proof_of_work(self, block, difficulty=2):
         proof = block.generate_hash()
--- script.py
+++ script.py
@@ -1,19 +1,20 @@
 from blockchain import Blockchain
 #Code done by LUCIA FUENTES, inspired by codecademy
 #testing
-block_one_transactions = {"sender":"Alice", "receiver": "Bob", "amount":"50"}
-block_two_transactions = {"sender": "Bob", "receiver":"Cole", "amount":"25"}
-block_three_transactions = {"sender":"Alice", "receiver":"Cole", "amount":"35"}
-fake_transactions = {"sender": "Bob", "receiver":"Cole, Alice", "amount":"25"}
+block_one_transactions = {"sender":"Lucia", "receiver": "Julia", "amount":"999"}
+block_two_transactions = {"sender": "Diego", "receiver":"Laika", "amount":"666"}
+block_three_transactions = {"sender":"Frida", "receiver":"Rex", "amount":"420"}
+fake_transactions = {"sender": "Renata", "receiver":"Jaime, Juan Pablo", "amount":"1398"}
 
 
-local_blockchain = Blockchain()
-local_blockchain.print_blocks()
+local_blockchain = Blockchain() #instattiation of a blockchain object
+local_blockchain.print_blocks() #print, to check that a default genesis block is created
 
+#create 3 blocks
 local_blockchain.add_block(block_one_transactions)
 local_blockchain.add_block(block_two_transactions)
 local_blockchain.add_block(block_three_transactions)
-local_blockchain.print_blocks()
+local_blockchain.print_blocks() #print local_clockchain, now should have 3 blocks + genesis block
 
-local_blockchain.chain[2].transactions = fake_transactions
-local_blockchain.validate_chain()
+local_blockchain.chain[2].transactions = fake_transactions #try to chnage one block data
+local_blockchain.validate_chain() #will notice the invalidity
